bdf13d839be2fe6e2627a5f9309dd136c976e1ea
ana2546 <aromero582@alumnos.iua.edu.ar>
2026-02-24 23:41:40 -0300

    handlebars(vistas),routing

--- backend-node-ts/src/index.ts
+++ backend-node-ts/src/index.ts
@@ -2,20 +2,43 @@
 // Esto nos permite tipar correctamente los parámetros de las rutas
 import express, { Request, Response } from 'express';
 import path from 'path';
+import { engine } from 'express-handlebars';
+import viewsRouter from './routes/views.routes';
+
 // Creamos la instancia principal de la aplicación Express
 const app = express();
 
 // Definimos el puerto donde va a escuchar el servidor
-const PORT = 3000;
+const PORT = 5000;
 
 // Middleware que permite leer JSON en el body de las requests
 app.use(express.json());
+
+
+// Configuración del motor de plantillas Handlebars
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
+
+
 
 // Middleware para servir archivos estáticos
 // __dirname representa la carpeta actual compilada 
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-// Endpoint GET raíz
+
+// Rutas de vistas
+app.use('/handlebars', viewsRouter);
+
+
+
+
+
+
+
+
+
+// Endpoint GET raíz  A partir de aca todos los endpoints van a irdesde /api/
 // URL: http://localhost:3000/
 app.get('/api', (req: Request, res: Response) => {
   // Respondemos con un objeto JSON simple
@@ -48,11 +71,7 @@ app.get('/api/usuario', (req: Request, res: Response<Usuario>) => {
   res.json(usuario);
 });
 
-// Iniciamos el servidor HTTP
-// Si todo está correcto, veremos el mensaje en consola
-app.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT}`);
-});
+ 
 
 
 
@@ -65,4 +84,18 @@ const usuarios: Usuario[] = [
 
 app.get('/api/usuarios', (req: Request, res: Response<Usuario[]>) => {
   res.json(usuarios);
+});
+
+
+app.get('/api/info', (req: Request, res: Response) => {
+  res.json({
+    info: "INFORMACIÓN AGREGADA POR LA API/INFO"
+  });
+});
+
+
+// Iniciamos el servidor HTTP
+// Si todo está correcto, veremos el mensaje en consola
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
